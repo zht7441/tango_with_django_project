8dca298e6a54c55fd10202364144b20fb2bb2273
Haotian Zheng <2568965Z@student.gla.ac.uk>
2021-07-27 18:39:27 +0800

    upload chapter4

--- rango/views.py
+++ rango/views.py
@@ -7,3 +7,21 @@ def index(request):
 
 def about(request):
  return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+
+def index(request):
+# Construct a dictionary to pass to the template engine as its context.
+# Note the key boldmessage matches to {{ boldmessage }} in the template!
+ context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+# Return a rendered response to send to the client.
+# We make use of the shortcut function to make our lives easier.
+# Note that the first parameter is the template we wish to use.
+ return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+# Construct a dictionary to pass to the template engine as its context.
+# Note the key boldmessage matches to {{ boldmessage }} in the template!
+ context_dict = {'boldmessage': 'This tutorial has been put together by Haotian Zheng'}
+# Return a rendered response to send to the client.
+# We make use of the shortcut function to make our lives easier.
+# Note that the first parameter is the template we wish to use.
+ return render(request, 'rango/about.html', context=context_dict)
