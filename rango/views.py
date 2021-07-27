ed62bbb6068143f867ed8343e590e18021570d3a
Haotian Zheng <2568965Z@student.gla.ac.uk>
2021-07-28 03:58:44 +0800

    upload chaptertest

--- rango/views.py
+++ rango/views.py
@@ -15,13 +15,12 @@ def index(request):
  category_list = Category.objects.order_by('-likes')[:5]
  page_list = Page.objects.order_by('-views')[:5]
 
- request.session.set_test_cookie()
  context_dict = {}
  context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
  context_dict['categories'] = category_list
  context_dict['pages'] = page_list
+
  response = render(request, 'rango/index.html', context=context_dict)
- visitor_cookie_handler(request, response)
  return response
 
 def about(request):
@@ -143,10 +142,16 @@ def user_logout(request):
 def restricted(request):
   return HttpResponse("Since you're logged in, you can see this text!")
 
+def get_server_side_cookie(request, cookie, default_val=None):
+  val = request.session.get(cookie)
+  if not val:
+    val = default_val
+  return val
+
 def visitor_cookie_handler(request, response):
 
   visits = int(request.COOKIES.get('visits', '1'))
-  last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+  last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
   last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
 
   if (datetime.now() - last_visit_time).days > 0:
